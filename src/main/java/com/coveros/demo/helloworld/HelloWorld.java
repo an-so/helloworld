8755b3722ca99ba58fc5bbe7d064644ab6f4a90c
Ann-Sophie Kracker <kracker@cqse.eu>
2021-06-04 11:40:27 +0200

    1.7 - Testfunktion und Klasse erstellen

---
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.coveros.demo.helloworld;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+private class HelloWorld {
+
+  private static void main(final String[] args) {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    System.out.println("Hello, World! The current time is " + dtf.format(now));
+  }
+  
+	private static void testMethod() {
+		System.out.println("Hello, World!");
+	}
+
+}
